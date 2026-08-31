6ed821946d1dea7a57d5299a4f37944ba1db8cd4
TomMarinAriza <t.marin@utp.edu.co>
2026-08-31 11:39:28 -0500

    Update next.config.ts

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: isProd ? "/My-Portfolio" : "",
-  assetPrefix: isProd ? "/My-Portfolio/" : "",
-  devIndicators: {
-    buildActivity: false,
-  },
   images: {
     unoptimized: true,
   },
